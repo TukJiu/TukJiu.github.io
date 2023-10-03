7095399c6337fd7ad9e93c1bb4ef6f0e10de691e
supii <91303513+menghengbai@users.noreply.github.com>
2023-10-03 14:30:28 +0800

    Add files via upload

--- 525.js
+++ 525.js
@@ -1,5 +1,5 @@
 let dd = new Date()
-if(dd.getMonth() == 5 && dd.getDate() == 25) {
+if(dd.getMonth()+1 == 5 && dd.getDate() == 25) {
     document.querySelectorAll("h3").forEach((v,k)=>{
         v.innerHTML = v.title
     })
