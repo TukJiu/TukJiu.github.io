198a5b1703eb14f10258a271edbd2864c08974e8
Nuxgo <91303513+TukJiu@users.noreply.github.com>
2024-03-04 20:22:09 +0800

    Delete 525.js

--- 525.js
+++
@@ -1,6 +0,0 @@
-let _525Date = new Date()
-if(_525Date.getMonth()+1 == 5 && _525Date.getDate() == 25) {
-    document.querySelectorAll("h3").forEach((v,k)=>{
-        v.innerHTML = v.title
-    })
-}
